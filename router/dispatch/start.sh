66dca4062dcab5100a0d3959fbde8ba974f96ac6
Jakub Scholz <www@scholzj.com>
2017-05-21 01:26:55 +0200

    Add WS gateway to start and stop scripts

--- router/dispatch/start.sh
+++ router/dispatch/start.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+docker rm router
+docker rm wsgw
 docker run -d -p 5672:5672 --name router scholzj/building-services-and-apis-using-amqp:latest
+docker run -d -p 5673:5673 --name wsgw --link router:router scholzj/qpid-dispatch:0.8.0 websockify 0.0.0.0:5673 router:5672
--- router/dispatch/stop.sh
+++ router/dispatch/stop.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 docker stop router
+docker stop wsgw
+docker rm router
+docker rm wsgw
